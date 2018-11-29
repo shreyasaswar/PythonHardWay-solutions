de978b45fe9642f2f13ce226294222eb34e7b76c
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 12:11:01 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,17 @@
+a = 'Shreyas Aswar'
+my_age = 24
+my_height = 5.9
+my_weight = 75
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print(f"So my name is {a}")
+print(f"and I'm {my_age} years old")
+print(f"if you measure me it'll be {my_height} ft")
+print(f"and im {my_weight} kg")
+print(f"i have {my_eyes}eyes and {my_teeth} teeths")
+print(f"i have {my_hair} hair")
+
+t = my_age + my_weight + my_height
+print(f"if you add {my_age},{my_height} and {my_weight} you will get {t}.")
