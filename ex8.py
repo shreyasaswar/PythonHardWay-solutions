9d517e817a3db3be5cd98492bc3563f29968b682
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 18:08:01 +0530

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,11 @@
+formater = "{} {} {} {}"
+
+print(formater.format(1,2,3,4))
+print(formater.format("one","two","three","four"))
+print(formater.format(True, False, False, True))
+print(formater.format(formater,formater,formater,formater))
+
+print(formater.format("\n \tAe mere watan k logo.. ",
+                        "\n \tJara aankh me bharlo pani ..",
+                        "\n \tJo shahid hue hai unki..",
+                        "\n \tJara yaad kro kurbani !!\n"))
