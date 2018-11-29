0145a27c0eb3339792a2e46573d78d9135a680a5
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 18:01:27 +0530

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print("Shreyas had a little chair.")
+print("It's color was blue as {}.".format('sky'))
+print("And some random statement.")
+print("=" *15) # This prints '=' 15 times
+
+end1 = "S"
+end2 = "h"
+end3 = "r"
+end4 = "e"
+end5 = "y"
+end6 = "a"
+end7 = "s"
+end8 = "A"
+end9 = "s"
+end10 = "w"
+end11 = "a"
+end12 = "r"
+
+#We have seen concatenation, but the last end defines how the print statement should end
+print(end1 + end2 + end3 + end4 + end5 + end6 + end7, end=' ')
+print(end8 + end9 + end10 +end11 + end12) #Bydefault it is \n
