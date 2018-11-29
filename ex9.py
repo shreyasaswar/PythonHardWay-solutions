7df4a783b081a18470cdf9680ff86ec0beea1e2d
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 18:11:15 +0530

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+Months = "Jan \n Feb \n March \nApril\n June\nJuly\nAug\nSept\nOct\nNov\nDec"
+
+print("Here are the days", days)
+print("Here are the months", Months)
+
+#It is multiline print statement , which uses triple quotes
+print("""
+This is the first time im using triple time double quotes
+which is fun
+may be its unformatted text which        it will print
+""")
+#P.S. - This is a old snippet , thats why it says first time using the triple quotes.
