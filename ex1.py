054463f40e917adbc5ca8996596a25192ef621b7
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 11:33:59 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#We can comment the code using the symbol octothorpe
+print("as usual Hello world!")
+print("hello again")
+print("i dont like typing hello words")
+print("this is irritating")
+print("wth is this")
+# we used single quotes inside double
+print("I'd much rather you 'not'.")
+print('I "said" do nothis  touch t.')
+print('is it printing?')
+#here is multiline printing using print() , notice the triple quotes ?
+print('''So here we learn that the syntax of print statement and how qoutes tell
+python what to print.
+''')
