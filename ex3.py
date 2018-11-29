a786304ff30693c2c72dd5805915b8789a17afe8
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 11:54:02 +0530

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my macbooks:")
+#prints MacBooks , result
+print("MacBooks", 25+30/6)
+print("Ipads", 100-25*3%4) #first multiplication then modulus then substraction
+
+print("Now i will count the Iphones:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3+2 < 5-7?")
+#use of boolean expression
+print(3+2 <5-7)
+#boolean expression prints either true or false
+print("what is 3 +2?",3+2)
+print("what is 5-7?",5-7)
+
+print("ohh, that's why it's false")
+
+print("how about some more")
+#So we can use maths in print statements !
+print("is it greater?", 5>-2)
+print("is it greater or equal?", 5>=-2)
+print("is it less or equal?",5<=-2)
