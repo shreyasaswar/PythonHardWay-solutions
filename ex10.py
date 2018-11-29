6c24775eb338f555ca452d5aff0a5b55b3db81fb
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 18:17:29 +0530

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,19 @@
+#\t is for paragraph or tab
+puneri_manjar = "\t I'm egoistic."
+#\n is for newline
+nagpuri_manjar = "I'm split\non line."
+#here \\ is for escaping the backslash
+kokni_manjar = "I'm \\ a \\ cat."
+
+#here is multiline print statement
+jadi_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Mashya
+\t* Catnip\n\t* Gavat
+'''
+
+print(puneri_manjar)
+print(nagpuri_manjar)
+print(kokni_manjar)
+print(jadi_cat)
