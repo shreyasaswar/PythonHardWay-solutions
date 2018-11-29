c414accfe896f1e867e89b6f1ed7f003e6d12ce1
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 18:31:17 +0530

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,6 @@
+#Here we just used input by promt 
+age = int(input("What is your age ?"))
+height = input("How tall are you ?")
+wt = input('how much you weigh?')
+
+print(f"So your age is {age} with {height} height and you are {wt} heavy.")
