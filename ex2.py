7a6ea47b5361e0af6b139628cd8fbdc08eaeba31
shreyasaswar <44370876+shreyasaswar@users.noreply.github.com>
2018-11-29 11:46:56 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# This snippet is all about comments #shreyas
+# It tells what a comment does and what are different uses of it
+#like comment can share some information
+print("My name is shreyas")
+#print("my name is not shreyas")
+# see how we comment out the code , to disable that part
+print("I'm shreyas again") #we can comment here too 
